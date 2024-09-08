c88d1c0d9ff15479691493786f7f4b1936e95be4
mman755 <mman755@aucklanduni.ac.nz>
2024-09-08 17:12:01 +1200

    Remove encoding from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,6 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        -- Ensure global status line is enabled
         vim.o.laststatus = 3 -- This makes the status line global
 
         require('lualine').setup {
@@ -28,7 +27,7 @@ return {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_x = { 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
             },
