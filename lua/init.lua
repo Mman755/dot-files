e980e08dd9dc237c4516558ecd07ce3ba113939a
mman755 <mman755@aucklanduni.ac.nz>
2024-09-08 17:10:22 +1200

    Add lualine plugin

--- lua/init.lua
+++ lua/init.lua
@@ -20,6 +20,3 @@ autocmd('LspAttach', {
     end
 })
 
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,50 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        -- Ensure global status line is enabled
+        vim.o.laststatus = 3 -- This makes the status line global
+
+        require('lualine').setup {
+            options = {
+                icons_enabled = true,
+                theme = 'auto', -- Choose your preferred theme
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = true, -- Use global status line
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                }
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
+    end
+}
+
