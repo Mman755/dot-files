3ea792cab3d2b9d9ce33a11f5ff4555fe7114126
mman755 <mman755@aucklanduni.ac.nz>
2024-09-09 22:59:28 +1200

    Make python LSP work for both projects & standalone scripts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,22 +39,18 @@ return {
                     }
                 end,
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
+		["pyright"] = function()
+		    local lspconfig = require("lspconfig")
+		    lspconfig.pyright.setup {
+			capabilities = capabilities,
+	                root_dir = function(fname)
+		        local util = require("lspconfig.util")
+			-- Use the current file's directory as the root
+			return util.find_git_ancestor(fname) or util.path.dirname(fname)
+		    end,
+			}
+		end,
 
-                end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
